40b095630fbccb5f3dfb7a8eda37249dcb0ba0df
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-21 09:27:35 +0000

    login component created with oauth functionalities

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators, FormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AuthServiceService } from '../../service/auth/auth-service.service';
 import { Login } from '../../model/login';
 import { SpinnerComponent } from '../../components/spinner/spinner.component';
 import { CommonModule } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, SpinnerComponent, CommonModule, RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, SpinnerComponent, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm: FormGroup
   isLoading: boolean = false
 
-  constructor(private as: AuthServiceService, private fb: FormBuilder, private toastr: ToastrService, private router: Router ) {
+  constructor(
+    private as: AuthServiceService, 
+    private fb: FormBuilder, 
+    private toastr: ToastrService, 
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     })
+  }
+
+  ngOnInit(): void {
+      this.route.queryParams.subscribe(params => {
+        if(params['token']) {
+          this.as.handleOAuthCallback(params)
+        }
+      })
   }
 
   login(): void {
@@ -40,17 +54,25 @@ export class LoginComponent {
 
     this.as.login(user).subscribe({
       next: () => {
-        this.toastr.success('Please wait!', 'Login successful')
+        setTimeout(() => [
+          this.toastr.success('Please wait!', 'Login successful')
+        ], 1000)
         setTimeout(() => {
           this.router.navigate([''])
-        }, 4000)
+        }, 6000)
       },
       error: () => {
         setTimeout(() => {
           this.isLoading = false
         }, 2000)
-        this.toastr.error('Invalid email or password', 'error')
+        setTimeout(() => {
+          this.toastr.error('Invalid email or password', 'error')
+        }, 2000)
       }
     })
+  }
+
+  googleLogin(): void {
+    this.as.googleOAuth()
   }
 }
