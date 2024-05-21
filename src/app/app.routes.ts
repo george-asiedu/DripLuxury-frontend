a206a9f944f0abc560cfbe85b065f9061b0ddcd6
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-21 09:21:34 +0000

    application routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,36 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { VerificationComponent } from './pages/verification/verification.component';
+import { ChangePasswordComponent } from './pages/change-password/change-password.component';
 
 export const routes: Routes = [
     {
         path: 'signup',
-        title: 'DripLuxury Clothing - Signup',
+        title: 'DripLuxury Clothing | Signup',
         component: SignupComponent
     },
     {
         path: 'login',
-        title: 'DripLuxury Clothing - Login',
+        title: 'DripLuxury Clothing | Login',
         component: LoginComponent
-    }, 
+    },
+    {
+        path: 'reset-password',
+        title: 'DripLuxury Clothing | Reset Password',
+        component: ResetPasswordComponent
+    } ,
+    {
+        path: 'verify-code',
+        title: 'DripLuxury Clothing | Verify Code',
+        component: VerificationComponent
+    },
+    {
+        path: 'change-password',
+        title: 'DripLuxury Clothing | Change Password',
+        component: ChangePasswordComponent
+    },
     {
         path: '',
         title: 'DripLuxury Clothing - Home',
