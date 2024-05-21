7ba0eba0de1b7c407fa18606afc8ffd6d2e2fb69
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-05-21 09:23:45 +0000

    products interface defined

---
+++ src/app/model/products.ts
@@ -0,0 +1,13 @@
+export interface Products {
+    id: number,
+    name: string,
+    productImage: string,
+    price: number,
+    description: string,
+    category: string,
+    tags: string,
+    rating: number,
+    dressStyle: string,
+    colors: string[],
+    size: string[]
+}
