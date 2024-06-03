deca6d3e97491145f751e0729a1e54015c8666ca
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-03 09:24:56 +0000

    men home component created

---
+++ src/app/pages/home/men-home/men-home.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Products } from '../../../model/products';
+import { ProductsServiceService } from '../../../service/products/products-service.service';
+import { NgFor } from '@angular/common';
+import { ProductCardComponent } from '../../../components/product-card/product-card.component';
+
+@Component({
+  selector: 'app-men-home',
+  standalone: true,
+  imports: [NgFor, ProductCardComponent],
+  templateUrl: './men-home.component.html',
+  styleUrl: './men-home.component.scss'
+})
+export class MenHomeComponent {
+  products: Products[] = []
+
+  constructor(private productService: ProductsServiceService) {}
+
+  ngOnInit(): void {
+      this.productService.getMenProducts(14).subscribe({
+        next: (response: Products[]) => {
+          this.products = response.filter(product => product.type !== 'new in' && product.category.includes('men'))
+        },
+        error: (err: any) => {
+          console.error('Error fetching products', err)
+        }
+      })
+  }
+}
