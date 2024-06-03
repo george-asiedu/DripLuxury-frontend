6e79ac1d5d5116203f955114ce95f5c8c46c5d7f
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-03 09:25:14 +0000

    new arrivals component updated

--- src/app/pages/home/new-arrivals/new-arrivals.component.ts
+++ src/app/pages/home/new-arrivals/new-arrivals.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Products } from '../../../model/products';
 import { ProductsServiceService } from '../../../service/products/products-service.service';
-import { Router } from '@angular/router';
+import { ProductCardComponent } from '../../../components/product-card/product-card.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-new-arrivals',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, NgForOf],
   templateUrl: './new-arrivals.component.html',
   styleUrl: './new-arrivals.component.scss'
 })
-export class NewArrivalsComponent implements OnInit {
+export class NewArrivalsComponent {
   products: Products[] = []
 
-  constructor(private productService: ProductsServiceService, private router: Router) {}
+  constructor(private productService: ProductsServiceService) {}
 
   ngOnInit(): void {
       this.productService.getAllProducts().subscribe({
-        next: (response) => {
-          this.products = response
+        next: (response: Products[]) => {
+          this.products = response.filter(product => product.type.includes('new in'))
         },
         error: (err) => {
           console.error('Error fetching products', err)
