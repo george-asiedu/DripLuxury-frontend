68cc37296ff995a756b37cf0dbdf67acf37a7786
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-06-05 18:19:16 +0000

    app.routes updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,13 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
 import { VerificationComponent } from './pages/auth/verification/verification.component';
 import { ChangePasswordComponent } from './pages/auth/change-password/change-password.component';
-import { SalesComponent } from './pages/category/men/sales/sales.component';
 import { WomenClothingComponent } from './pages/category/women/women-clothing/women-clothing.component';
 import { KidsClothingComponent } from './pages/category/kids/kids-clothing/kids-clothing.component';
+import { NewInComponent } from './components/new-in/new-in.component';
+import {MenCategoryComponent} from "./pages/category/men/men-category/men-category.component";
+import {MenCategoryHomeComponent} from "./pages/category/men/men-category-home/men-category-home.component";
+import {SalesComponent} from "./components/sales/sales.component";
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 export const routes: Routes = [
     {
@@ -43,7 +47,12 @@ export const routes: Routes = [
     {
         path: 'men-category',
         title: 'DripLuxury Clothing | Men Home',
-        component: SalesComponent
+        component: MenCategoryComponent,
+        children: [
+          { path: '', component: MenCategoryHomeComponent },
+          { path: 'sales', component: SalesComponent },
+          { path: 'new-in', component: NewInComponent }
+        ]
     },
     {
         path: 'women-category',
@@ -54,5 +63,15 @@ export const routes: Routes = [
         path: 'kids',
         title: 'DripLuxury Clothing | Kids Home',
         component: KidsClothingComponent
+    },
+    {
+        path: ':id',
+        title: 'DripLuxury Clothing | Product Details Page',
+        component: ProductDetailsComponent
+    }, 
+    {
+        path: '**',
+        redirectTo: '',
+        component: HomeComponent
     }
 ]
